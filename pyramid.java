80293008afe4e9a8e6aac46a9c93e8457f0a1148
Vinay Kumar K N <159755384+vinaykumarkn001@users.noreply.github.com>
2024-02-12 07:10:45 +0000

    Java pyramid program

---
+++ pyramid.java
@@ -0,0 +1,91 @@
+
+import java.util.Scanner;
+
+class pyramid {
+
+	public static void main(String[] args) {
+//    pyramid();
+//    halfpyramid();
+    diamond(12);		
+		
+	}
+
+	private static void diamond(int rows) {
+		int r=rows;
+		int st=1, s=r-1;
+		for(int i=1; i<=r; i++)
+		{
+			for(int j=1; j<=s; j++)
+			{
+			System.out.print(" ");
+			}
+			
+			for(int k=1; k<=st; k++)
+			{
+				System.out.print("*");
+			}
+			st=st+2;
+			System.out.println();
+			s--;
+		}
+		
+		st=st-4;
+		r=r-1;
+		s=1;
+		for(int i=1; i<=r; i++)
+		{
+			for(int j=1; j<=s; j++)
+			{
+			System.out.print(" ");
+			}
+			for(int k=1; k<=st; k++)
+			{
+				System.out.print("*");
+			}	
+			st=st-2;
+			System.out.println();
+			s++;
+		}
+	}
+
+	private static void halfpyramid() {
+		int r=5;
+		int st=1, s=r-1;
+		for(int i=1; i<=r; i++)
+		{
+			for(int j=1; j<=s; j++)
+			{
+			System.out.print(" ");
+			}
+			
+			for(int k=1; k<=st; k++)
+			{
+				System.out.print("*");
+			}
+			st=st+1;
+			System.out.println();
+			s--;
+		}
+	}
+
+	private static void pyramid() {
+		int r=5;
+		int st=1, s=r-1;
+		for(int i=1; i<=r; i++)
+		{
+			for(int j=1; j<=s; j++)
+			{
+			System.out.print(" ");
+			}
+			
+			for(int k=1; k<=st; k++)
+			{
+				System.out.print("*");
+			}
+			st=st+2;
+			System.out.println();
+			s--;
+		}
+	}
+	
+}
